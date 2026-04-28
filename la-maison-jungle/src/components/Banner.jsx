460dd16539610b721b12858c4d37cc829d190763
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2026-04-28 16:36:28 +0300

    J' exporte le composant Banner utilisant les mots-clés export default suivi du nom du composant

---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,10 @@
+/**
+ *  Banner est le composant pour présenter la banniére ( la barre de navigation)
+ */
+const Banner = () => {
+    // Le composant Banner retourne un message code en HTML 
+    return <h1> Bienvenue à la maison Jungle </h1>
+}
+
+// J' exporte le composant Banner utilisant les mots-clés export default suivi du nom du composant
+export default Banner 
