0a6bd93e622c1c4e5874bafad13c85d41f7bb3d5
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2026-04-29 16:13:22 +0300

    utilise un CSS Module objet styles importé depuis un fichier module.css

--- la-maison-jungle/src/components/Banner.jsx
+++ la-maison-jungle/src/components/Banner.jsx
@@ -3,19 +3,21 @@
  */
 
 // J'importe le fichier Banner.css appliqué au composant Banner.jsx
-import "../styles/Banner.css"
+import styles from "../styles/Banner.module.css"
 
+// J' importe le logo
 import logo  from '../assets/fleurs-logo.jpg';
 
 
 const Banner = () => {
     const title = "Bienvenu à la Maison Jungle"
     // Le composant Banner retourne un message code en HTML 
+    //utilise un CSS Module objet styles importé depuis un fichier module.css
+    //image importée en haut du fichier avec 
     return (
-        <div className="banner">
-            <img src={logo} alt="Logo La maison jungle" className="banner-logo" />
-            
-            <h1 className="banner-title"> {title} </h1>
+        <div className={styles.banner}>
+            <img src={logo} alt="Logo La maison jungle" className={styles.logo}/>
+            <h1 className={styles.titles}> {title} </h1>
         </div>
     )
 }
--- la-maison-jungle/src/components/Cart.jsx
+++ la-maison-jungle/src/components/Cart.jsx
@@ -1,22 +1,24 @@
-//Le composant cart représante le panier de courses 
-
-//import App from "../App"
+/**
+ * Le composant Cart représente le panier de courses.
+ * Le composant Cart sera réutilisé dans le composant App.
+ */
+import styles from '../styles/Cart.module.css'
 
 const Cart = () => {
-    const montsera = 8
+    const monstera = 8
     const lierre = 12
     const bouquet = 9
-    return (
-        <div>
-            <h2> Votre panier </h2>
-            <ul>
-                <li> Montsera : {montsera} € </li>
-                <li> Lierre : {lierre} $ </li>
-                <li> Bouquet de fleurs : {bouquet} € </li>
+
+    return(
+        <div className={styles.cart}>
+            <h2>Votre panier</h2>
+            <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+                <li>Monstera : {monstera} €</li>
+                <li>Lierre : {lierre} €</li>
+                <li>Bouquet de fleurs : {bouquet} €</li>
             </ul>
-            <p> Total : {montsera + lierre + bouquet} € </p>
+            <p>Total : {monstera + lierre + bouquet} €</p>
         </div>
-        
     )
 }
 
